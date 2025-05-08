4cd8b92b4f95b243ca7d318a778e6278ffe0f185
Thibault <>
2025-05-08 22:46:27 +0200

    Characters Scripts & Images

---
+++ Genshin/characters_icon.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import os
+import requests
+
+# with open('Genshin/characters.html', 'w', encoding='utf-8') as f:
+#     url = 'https://genshin-impact.fandom.com/wiki/Character/List'
+#     r = requests.get(url)
+#     f.write(r.text)
+
+f = open('Genshin/characters.html', encoding='utf8')
+soup = BeautifulSoup(f, 'html.parser')
+for character in soup.find('table').find_all('tr')[1:] :
+    character_td_img = character.find('td')
+    character_img = character_td_img.find('img')['data-src'].split('/scale-to-width-down')[0]
+    character_td_name = character_td_img.next_sibling.next_sibling
+    character_name = character_td_name.find('a').get_text()
+    character_td_element = character_td_name.next_sibling.next_sibling.next_sibling.next_sibling
+    if (character_td_element.get_text().strip() == 'None') :
+        character_element = 'None'
+    else :
+        character_element = character_td_element.find_all('a')[1].get_text()
+    image_location = 'Genshin/Characters/'+character_element+'/'+character_name+'.png'
+
+    if (not os.path.isfile(image_location)) :
+        img_data = requests.get(character_img).content
+        with open(image_location, 'wb') as handler:
+            handler.write(img_data)
---
+++ Genshin/characters_info.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+import json
+import requests
+
+# with open('Genshin/characters.html', 'w', encoding='utf-8') as f:
+#     url = 'https://genshin-impact.fandom.com/wiki/Character/List'
+#     r = requests.get(url)
+#     f.write(r.text)
+
+with open('Genshin/characters.json') as json_file:
+    characters = json.load(json_file)
+
+f = open('Genshin/characters.html', encoding='utf8')
+soup = BeautifulSoup(f, 'html.parser')
+for character in soup.find('table').find_all('tr')[1:] :
+    character_td_name = character.find('td').next_sibling.next_sibling
+    character_name = character_td_name.find('a').get_text()
+    character_link = 'https://genshin-impact.fandom.com'+character_td_name.find('a')['href']
+
+    character_td_quality = character_td_name.next_sibling.next_sibling
+    character_quality = character_td_quality.find('img')
+    character_quality = character_quality['alt'].replace(' Stars', '')
+
+    character_td_element = character_td_quality.next_sibling.next_sibling
+    if (character_td_element.get_text().strip() == 'None') :
+        character_element = 'None'
+    else :
+        character_element = character_td_element.find_all('a')[1].get_text()
+
+    character_td_weapon = character_td_element.next_sibling.next_sibling
+    character_weapon = character_td_weapon.find_all('a')[1].get_text()
+
+    if character_name not in characters[character_element] :
+        characters[character_element][character_name] = {
+            'quality' : character_quality,
+            'weapon' : character_weapon,
+            'link' : character_link,
+            'src' : 'Genshin/Characters/'+character_element+'/'+character_name+'.png'
+        }
+
+with open('Genshin/characters.json', 'w') as file:
+    file.write(json.dumps(characters, indent=4))
