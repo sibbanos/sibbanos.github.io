e1ecaaaea8b17ec9d201efe37b95b6323396750e
Thibault <>
2025-05-08 22:49:45 +0200

    Weapons Scripts & Images

---
+++ Genshin/weapons_icon.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import os
+import requests
+
+# with open('Genshin/weapons.html', 'w', encoding='utf-8') as f:
+#     url = 'https://genshin-impact.fandom.com/wiki/Weapon/List/By_Weapon_Type'
+#     r = requests.get(url)
+#     f.write(r.text)
+
+f = open('Genshin/weapons.html', encoding='utf8')
+soup = BeautifulSoup(f, 'html.parser')
+for weapon_type in soup.find_all('span', 'mw-headline') :
+    weapon_type_name = weapon_type.get_text()
+    weapon_list = weapon_type.parent.next_sibling.next_sibling.next_sibling.next_sibling.find_all('tr')
+
+    for weapon in weapon_list[1:] :
+        weapon_img = weapon.find('img')['data-src'].split('/scale-to-width-down')[0]
+        weapon_name = weapon.find('img').parent['title'].replace('"', '')
+        image_location = 'Genshin/Weapons/'+weapon_type_name+'/'+weapon_name+'.png'
+
+        if (not os.path.isfile(image_location)) :
+            img_data = requests.get(weapon_img).content
+            with open(image_location, 'wb') as handler:
+                handler.write(img_data)
---
+++ Genshin/weapons_info.py
@@ -0,0 +1,60 @@
+from bs4 import BeautifulSoup
+import json
+import requests
+import re
+
+# with open('Genshin/weapons.html', 'w', encoding='utf-8') as f:
+#     url = 'https://genshin-impact.fandom.com/wiki/Weapon/List/By_Weapon_Type'
+#     r = requests.get(url)
+#     f.write(r.text)
+
+with open('Genshin/weapons.json') as json_file:
+    weapons = json.load(json_file)
+
+f = open('Genshin/weapons.html', encoding='utf8')
+soup = BeautifulSoup(f, 'html.parser')
+for weapon_type in soup.find_all('span', 'mw-headline') :
+    weapon_type_name = weapon_type.get_text()
+    weapon_list = weapon_type.parent.next_sibling.next_sibling.next_sibling.next_sibling.find_all('tr')
+
+    for weapon in weapon_list[1:] :
+        weapon_td_name = weapon.find('td').next_sibling.next_sibling
+        weapon_name = weapon_td_name.find('a').get_text().replace('"', '')
+        weapon_link = 'https://genshin-impact.fandom.com'+weapon_td_name.find('a')['href']
+
+        weapon_td_quality = weapon_td_name.next_sibling.next_sibling
+        weapon_quality = weapon_td_quality.find('img')
+        weapon_quality = weapon_quality['alt'].replace(' Stars', '')
+
+        weapon_td_first_stat = weapon_td_quality.next_sibling.next_sibling
+        weapon_first_stat = weapon_td_first_stat.get_text().split('(')[0]
+
+        weapon_td_second_stat = weapon_td_first_stat.next_sibling.next_sibling
+
+        if (weapon_td_second_stat.get_text().strip() != 'None') :
+            weapon_second_stat_type = re.split(' \\d', weapon_td_second_stat.get_text())[0]
+            weapon_second_stat = re.findall('\\d[^( ]*', weapon_td_second_stat.get_text())[0]
+        else :
+            weapon_second_stat_type = ''
+            weapon_second_stat = ''
+
+        weapon_td_ability = weapon_td_second_stat.next_sibling.next_sibling
+        for br in weapon_td_ability.find_all('br'):
+            br.replace_with("\n")
+        weapon_ability = weapon_td_ability.get_text().strip()
+        if (weapon_ability == 'None') :
+            weapon_ability = ''
+
+        if weapon_name not in weapons[weapon_type_name] :
+            weapons[weapon_type_name][weapon_name] = {
+                'quality' : weapon_quality,
+                'first_stat' : weapon_first_stat,
+                'second_stat' : weapon_second_stat,
+                'second_stat_type' : weapon_second_stat_type,
+                'ability' : weapon_ability,
+                'link' : weapon_link,
+                'src' : 'Genshin/Weapons/'+weapon_type_name+'/'+weapon_name+'.png'
+            }
+
+with open('Genshin/weapons.json', 'w') as file:
+    file.write(json.dumps(weapons, indent=4))
